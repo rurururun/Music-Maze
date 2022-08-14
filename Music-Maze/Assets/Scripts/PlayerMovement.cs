1c3419cfe00fd889f2b9a2ddc8dd971a3efa87d1
Chua Guo Heng <guohengchua@gmail.com>
2022-08-14 22:52:33 +0800

    Added restrictions to the player movement, added death of player if hit wall, added main menu with play and exit button both working, added bgm for main menu

--- Music-Maze/Assets/Scripts/PlayerMovement.cs
+++ Music-Maze/Assets/Scripts/PlayerMovement.cs
@@ -1,38 +1,80 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
 
 public class PlayerMovement : MonoBehaviour
 {
     // Variable for movement
+    bool canMoveLeft;
+    bool canMoveRight;
+    bool canMoveUp;
+    bool canMoveDown;
     Rigidbody2D player;
+    GameObject[] wall;
     //Collider2D bodyCollider;
+
+    // Fail message
+    public TextMeshProUGUI failMessage;
 
     // Start is called before the first frame update
     void Start()
     {
+        canMoveLeft = true;
+        canMoveRight = true;
+        canMoveUp = true;
+        canMoveDown = true;
         player = GetComponent<Rigidbody2D>();
+        wall = GameObject.FindGameObjectsWithTag("Wall");
         //bodyCollider = GetComponent<Collider2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.A))
+        // Checking if player collided with wall
+        foreach (GameObject w in wall)
         {
-            player.position = new Vector2(player.position.x - 5, player.position.y);
+            if (player.IsTouching(w.GetComponent<Collider2D>()))
+            {
+                canMoveLeft = false;
+                canMoveRight = false;
+                canMoveUp = false;
+                canMoveDown = false;
+                failMessage.gameObject.SetActive(true);
+                player.gameObject.SetActive(false);
+            }
         }
-        else if (Input.GetKeyDown(KeyCode.D))
+
+        // Movement and restricting player from moving back to their previous position
+        if (Input.GetKeyDown(KeyCode.A) && canMoveLeft)
         {
-            player.position = new Vector2(player.position.x + 5, player.position.y);
+            player.position = new Vector2(player.position.x - 2, player.position.y);
+            canMoveRight = false;
+            canMoveUp = true;
+            canMoveDown = true;
         }
-        else if (Input.GetKeyDown(KeyCode.S))
+        else if (Input.GetKeyDown(KeyCode.D) && canMoveRight)
         {
-            player.position = new Vector2(player.position.x, player.position.y - 5);
+            player.position = new Vector2(player.position.x + 2, player.position.y);
+            canMoveLeft = false;
+            canMoveUp = true;
+            canMoveDown = true;
         }
-        else if (Input.GetKeyDown(KeyCode.W))
+        else if (Input.GetKeyDown(KeyCode.S) && canMoveDown)
         {
-            player.position = new Vector2(player.position.x, player.position.y + 5);
+            player.position = new Vector2(player.position.x, player.position.y - 2);
+            canMoveUp = false;
+            canMoveRight = true;
+            canMoveLeft = true;
+        }
+        else if (Input.GetKeyDown(KeyCode.W) && canMoveUp)
+        {
+            player.position = new Vector2(player.position.x, player.position.y + 2);
+            canMoveDown = false;
+            canMoveRight = true;
+            canMoveLeft = true;
         }
     }
 }
