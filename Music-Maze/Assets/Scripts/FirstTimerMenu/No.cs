80826879c30bd255c2de863a5ebb91e91343b8e4
alfianlion <alfianlion@gmail.com>
2022-08-15 14:55:18 +0800

    Completed Milestone: Pop-up for first-timers

---
+++ Music-Maze/Assets/Scripts/FirstTimerMenu/No.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class No : MonoBehaviour
+{
+
+    public void NoButton()
+    {
+        SceneManager.LoadScene(3);
+    }
+
+}
---
+++ Music-Maze/Assets/Scripts/FirstTimerMenu/Yes.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Yes : MonoBehaviour
+{
+    
+    public void YesButton()
+    {
+        SceneManager.LoadScene(2);
+    }
+    
+}
